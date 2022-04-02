2788f93ac62f0357b8097683ce2b410912dc4cec
GO-Shoderu <go.shoderu@gmail.com>
2022-04-02 10:56:35 +0200

    Changes to the product-details files and also including comments to each line of the code that could be confusing to me later on
    Signed-off-by: GO-Shoderu <go.shoderu@gmail.com>

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 
+//importing these in order to display each product based on the url of the browser
+import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
 
 import { CartService } from '../cart.service';
@@ -11,17 +12,23 @@ import { CartService } from '../cart.service';
   styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
+
+  // defining the product property here
   product: Product | undefined;
 
+  // injecting the constructor as well as declaring the variable and defining it
   constructor(
     private route: ActivatedRoute,
     private cartService: CartService
   ) {}
 
   ngOnInit(): void {
+
+    // getting product id from the current route using these
     const routeParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routeParams.get('productId'));
 
+    //using the id provided in the route to find the product that corresponds with the id
     this.product = products.find(
       (product) => product.id === productIdFromRoute
     );
